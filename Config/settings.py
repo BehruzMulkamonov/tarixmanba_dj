46985b26ae6a759348e39e21881b8ed1b7d78781
asadbek000002 <asadbektuygunov9@gmail.com>
2024-07-31 20:57:15 +0500

    static file in docker

--- Config/settings.py
+++ Config/settings.py
@@ -142,6 +142,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
