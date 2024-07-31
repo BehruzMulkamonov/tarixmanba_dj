73dc5e203f9c93c09cc4470be67c7544c6a682ab
asadbek000002 <asadbektuygunov9@gmail.com>
2024-07-31 21:25:58 +0500

    data baza

--- Config/settings.py
+++ Config/settings.py
@@ -86,23 +86,23 @@ WSGI_APPLICATION = 'Config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'tarixmanba_db',
-#         'USER': 'tarixmanba_user',
-#         'PASSWORD': 'tarixmanba_password',
-#         'HOST': 'db',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tarixmanba_db',
+        'USER': 'tarixmanba_user',
+        'PASSWORD': 'tarixmanba_password',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
